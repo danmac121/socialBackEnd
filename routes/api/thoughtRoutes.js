5948c19915a77acc6502a75d4720183f73aa0bcd
danmac121 <danmacdonald121@gmail.com>
2023-09-17 14:10:38 -0400

    added routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction
+} = require('../../controllers/thoughtController');
+
+// /api/Thoughts
+router.route('/')
+.get(getThoughts)
+.post(createThought);
+
+// /api/Thoughts/:ThoughtId
+router
+  .route('/:ThoughtId')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// /api/Thoughts/:ThoughtId/reactions
+router.route('/:thoughtId/reactions')
+.post(addReaction);
+
+// /api/Thoughts/:ThoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId')
+.delete(removeReaction);
+
+module.exports = router;
